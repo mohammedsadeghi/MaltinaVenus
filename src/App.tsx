39876060764c495ef34f96c4728553ce368013c7
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-05-08 23:00:29 +0430

    add Home view

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import {QueryClient, QueryClientProvider} from 'react-query';
-
+import {Home} from './views/Home';
 const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaView>
         <View>
-          <Text>hi</Text>
+          <Home />
         </View>
       </SafeAreaView>
     </QueryClientProvider>
