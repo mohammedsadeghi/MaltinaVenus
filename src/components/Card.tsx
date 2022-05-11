31dadf11c56d6a912b36f66e2d9f9b339a77c724
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-05-11 14:25:16 +0430

    add props type and type of React.FC & change some styles & fix bugs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,60 +1,65 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { Text, View, StyleSheet,Image } from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
+import {StarScore} from './StarScore';
 
-const styles = StyleSheet.create({
-  card: {
-    width:'100%',
-    height:'100px',
-    borderBottom: '1px solid #eaeaea',
-    padding:'0.5rem',
-    paddingRight:"1rem",
-    display: 'flex',
-    flexDirection: 'row-reverse',
-    justifyContent:"space-between",
-    alighItems:"center"
-  },
-  productImg: {
-    maxWidth: "6rem",
-    minWidth: "4rem",
-    height:"4rem",
-    margin:"auto"
-  },
-  title: {
-    width: "100%",
-    color:"#999",
-    lineHeight:"14px",
-    fontSize:"0.675rem",
-    marginTop:"0.25rem",
-    marginBottom:"0.25rem",
-    fontWieght:"100",
-  }
-})
+type cardInfoProps = {
+  [key: string]: any;
+};
 
-
-const Card= ({cardInfo}) => {
+const Card: React.FC<cardInfoProps> = ({cardInfo}) => {
   return (
     <View style={[styles.card]}>
-      <View>
+      <View style={styles.imageContainer}>
         <Image
           accessibilityLabel={cardInfo.title}
-        source={cardInfo.image}
-          resizeMode="contain"
+          source={{
+            uri: cardInfo.image,
+          }}
           style={styles.productImg}
+          resizeMode="contain"
         />
       </View>
-      <View style={{flex:1}}>
-        <Text>star component</Text>
-        <Text 
-          numberOfLines={2} 
-          style={[styles.title]}
-          >
+      <View style={{flex: 2}}>
+        <StarScore score={Number(cardInfo.review.stars)} />
+        <Text numberOfLines={2} style={[styles.title]}>
           {cardInfo.title}
-          </Text>
+        </Text>
         <Text>price component</Text>
       </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: '100%',
+    height: 120,
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderColor: '#eaeaea',
+    paddingHorizontal: 10,
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    paddingVertical: 8,
+  },
+  productImg: {
+    flex: 2,
+    width: 150,
+    height: 150,
+  },
+  title: {
+    textAlign: 'right',
+    width: '100%',
+    color: '#999',
+    lineHeight: 14,
+    marginTop: 3,
+    marginBottom: 3,
+  },
+});
 
 export default Card;
