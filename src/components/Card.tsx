4d9e88b9967ea57cb8fe10f181482678cd0813ef
unknown <Crypto.devloper3@gmail.com>
2022-05-11 17:35:55 +0430

    fix star component style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import {Text, View, StyleSheet, Image} from 'react-native';
 import {StarScore} from './StarScore';
-import Price from "./Price";
+import Price from './Price';
 
 type cardInfoProps = {
   [key: string]: any;
@@ -22,7 +22,9 @@ const Card: React.FC<cardInfoProps> = ({cardInfo}) => {
         />
       </View>
       <View style={{flex: 2}}>
-        {/* <StarScore score={Number(cardInfo.review.stars)} /> */}
+        <Text style={[styles.starComponent]}>
+          <StarScore score={Number(cardInfo.review.stars)} />
+        </Text>
         <Text numberOfLines={2} style={[styles.title]}>
           {cardInfo.title}
         </Text>
@@ -37,7 +39,7 @@ const Card: React.FC<cardInfoProps> = ({cardInfo}) => {
 const styles = StyleSheet.create({
   card: {
     width: '100%',
-    height: 120,
+    height: 150,
     backgroundColor: 'white',
     borderBottomWidth: 1,
     borderColor: '#eaeaea',
@@ -54,6 +56,7 @@ const styles = StyleSheet.create({
     flex: 2,
     width: 150,
     height: 150,
+    maxWidth: 100
   },
   title: {
     textAlign: 'right',
@@ -63,6 +66,10 @@ const styles = StyleSheet.create({
     marginTop: 3,
     marginBottom: 3,
   },
+  starComponent: {
+    position: 'relative',
+    bottom: 10
+  }
 });
 
 export default Card;
