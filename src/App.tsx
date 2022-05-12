5a3eedce0fea4f3ba3ad23c9336b65ef31dea59b
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-05-13 00:17:18 +0430

    change home view import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import {QueryClient, QueryClientProvider} from 'react-query';
-import {Home} from './views/Home';
+import Home from './views/Home';
 const queryClient = new QueryClient();
 const App = () => {
   return (
