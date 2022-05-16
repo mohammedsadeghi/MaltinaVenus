a0948897cdbfc9d4b453ed16593dcfb2c574408a
Marzieh Valizadeh <sureh1937@gmail.com>
2022-05-16 17:46:38 +0430

    fix: fix the switch-case of productsReducer

--- src/store/products/productsReducer.ts
+++ src/store/products/productsReducer.ts
@@ -19,9 +19,9 @@ const productsReducer = (state = initialState, action: reduxAction) => {
     case FETCH_PRODUCTS_REQUEST:
       return {...state, loading: true};
     case FETCH_PRODUCTS_SUCCESS:
-      return {...state, loading: false, error: action.payload, products: []};
-    case FETCH_PRODUCTS_FAILURE:
       return {...state, loading: false, error: '', products: action.payload};
+    case FETCH_PRODUCTS_FAILURE:
+      return {...state, loading: false, error: action.payload, products: []};
     default:
       return state;
   }
