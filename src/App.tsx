af26314a06d34b34a072642c98081c55b4e2998e
Marzieh Valizadeh <sureh1937@gmail.com>
2022-05-16 20:31:12 +0430

    feat: add store to src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import {Provider} from 'react-redux';
+import store from './store/store';
 import {Home} from './views/Home';
 const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <SafeAreaView>
-        <View>
-          <Home />
-        </View>
-      </SafeAreaView>
+      <Provider store={store}>
+        <SafeAreaView>
+          <View>
+            <Home />
+          </View>
+        </SafeAreaView>
+      </Provider>
     </QueryClientProvider>
   );
 };
